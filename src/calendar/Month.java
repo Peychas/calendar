6341f5983de2755281a9f7a464e12ea06e20adcc
Peychas <Nian_wow@live.se>
2016-11-22 15:09:58 +0100

    changed color on month

--- src/calendar/Month.java
+++ src/calendar/Month.java
@@ -3,14 +3,17 @@ package calendar;
 import java.awt.Color;
 import java.awt.GridLayout;
 
-import javax.swing.JLabel;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+
 
 public class Month extends JPanel {
 
 	public Month() {
 		setLayout(new GridLayout(4,4));
-		setBackground(Color.blue);
+		setBackground(Color.white);
+		
+		
 	}
 
 }
