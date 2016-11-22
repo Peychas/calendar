703a52379a3e96a2add221825afbfe975a0556e2
Jakob Sandstedt <sandstedt_95@hotmail.com>
2016-11-22 14:02:38 +0100

    inlogg
    Du vet

---
+++ src/calendar/Calendar.java
@@ -0,0 +1,42 @@
+package calendar;
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JSplitPane;
+import javax.swing.UIManager;
+
+public class Calendar extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	
+	private Inloggning inloggning;
+	
+	public Calendar()
+	{
+		super("Håll reda på dina prylar");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocation(350,25);
+		setPreferredSize(new Dimension(1200,1000));
+		try
+		{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch(Exception error){};
+		JSplitPane split = new JSplitPane();
+		setLayout(new GridLayout(1,1));
+		add(split);
+		inloggning = new Inloggning();
+		split.setLeftComponent(inloggning);
+		pack();
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new Calendar();
+
+	}
+
+}
---
+++ src/calendar/Inloggning.java
@@ -0,0 +1,28 @@
+package calendar;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class Inloggning extends JPanel {
+JPasswordField password;
+JTextField username;
+	
+		public Inloggning()
+		{
+			username = new JTextField();
+			username.setPreferredSize(new Dimension(100,30));
+			add(username);
+			setBackground(new Color(0,180,90));
+			password = new JPasswordField();
+			setLayout(new FlowLayout(FlowLayout.CENTER));
+			password.setPreferredSize(new Dimension(100,30));
+			add(password);
+			
+		}
+
+}
