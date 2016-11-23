03bf94009d471d7f294b692cc3941fb43f53383d
Peychas <Nian_wow@live.se>
2016-11-23 13:21:54 +0100

    week day labes added

--- src/calendar/Month.java
+++ src/calendar/Month.java
@@ -4,18 +4,24 @@ import java.awt.Color;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
 public class Month extends JPanel {
 
 	public Month() {
-		setBackground(Color.white);
-		setLayout(new GridLayout(5,7));
+		setLayout(new GridLayout(6,7));
+		JLabel[] labels = new JLabel[7];
+		String[] days = {"Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag"};
+		for(int i = 0; i < labels.length; i++){
+			labels[i] = new JLabel(days[i] , JLabel.CENTER);
+			add(labels[i]);
+		}
 		JButton[] buttons = new JButton[31];
 		for(int i = 0; i < buttons.length; i++){
-		buttons[i] = new JButton();
-		add(buttons[i]);
+			buttons[i] = new JButton();
+			add(buttons[i]);
 		}
 		
 	}
